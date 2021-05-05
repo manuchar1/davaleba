e1e6cfcd4c00e2d934409143ac9e65987afd57cd
Manuchar Zakariadze <zakariadzemanuchar@gmail.com>
2021-05-05 11:58:42 -0700

    დავალება

---
+++ src/MathOperations.kt
@@ -0,0 +1,55 @@
+class MathOperations {
+
+    fun greatestCommonDivisor(a: Int, b: Int): Int {
+        return if (b == 0) a else greatestCommonDivisor(b, a % b)
+
+    }
+
+    fun lowestCommonMultiple(a: Int, b: Int): Int {
+
+        return a * b / greatestCommonDivisor(a,b)
+    }
+
+
+
+    fun ifContain(value: String) {
+
+         if (value.contains("$")){
+             println("შეიცავს'$'სიმბოლოს")
+        }
+        else println("არ შეიცავს'$'სიმბოლოს")
+
+    }
+
+    fun sumEven(num1: Int, num2: Int): Int {
+        return if (num1 > num2) 0 else
+            num1 + sumEven(num1 + 2, num2)
+
+    }
+
+    fun reversedNum (){
+        var reversed = 0
+        var number = 456
+
+
+        while (number != 0) {
+            val digit = number % 10
+            reversed = reversed * 10 + digit
+            number /= 10
+        }
+
+
+    }
+
+    fun palindromeWords(word: String){
+        if (word.reversed() == word)
+            println("სტრინგი $word არის პალინდრომი")
+
+        else
+            println("სტრინგი $word არ არის პალინდრომი")
+
+
+    }
+
+
+}
---
+++ src/main.kt
@@ -0,0 +1,25 @@
+
+fun main(){
+    val result = MathOperations()
+
+    // უმაღლესი საერთო გამყოფი
+    println(result.greatestCommonDivisor(36,44))
+
+    // უმცირესი საერთო ჯერადი
+    println(result.lowestCommonMultiple(36,24))
+
+    // შეიცავს თუ არა $ სიმბოლოს
+    println(result.ifContain("word@#$"))
+
+    // 100 - ის ჩათვლით ყველა ლუწი რიცცხვის ჯამი
+    println(result.sumEven(2,100))
+
+    // შებრუნებული რიცხვი
+    println(result.reversedNum())
+
+    // სტრინგი არის თუ არა პალინდრომი
+    println(result.palindromeWords("racecar"))
+
+
+}
+
